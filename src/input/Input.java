958d3edee6aa2ddc50bc15062fe292e0aec672a9
Anthony13567 <44263545+Anthony13567@users.noreply.github.com>
2018-10-31 08:03:30 -0500

    Update Input.java

--- src/input/Input.java
+++ src/input/Input.java
@@ -24,16 +24,16 @@ public class Input {
           Scanner scan = new Scanner(new File("stuff.txt"));
           
           // Create an Array
-          String[] arr = new String[5];
+          String[] array = new String[5];
           
           // Take input line-by-line and store it in arr[]
-          arr[0] = scan.nextLine();
-          arr[1] = scan.nextLine();
-          arr[2] = scan.nextLine();
-          arr[3] = scan.nextLine();
-          arr[4] = scan.nextLine();
+          array[0] = scan.nextLine();
+          array[1] = scan.nextLine();
+          array[2] = scan.nextLine();
+          array[3] = scan.nextLine();
+          array[4] = scan.nextLine();
           
-          out.printf("%s %s %s %s %s", arr[0], arr[1], arr[2], arr[3], arr[4]);
+          out.printf("%s %s %s %s %s", array[0], array[1], array[2], array[3], array[4]);
           
           
           
